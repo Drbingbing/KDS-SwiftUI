d0a02b48a33654344f9596cfd042d6214bcb9256
Drbingbing <tw718140@gmail.com>
2024-10-01 14:51:39 +0800

    Initial Commit

---
+++ KDS-SwiftUI/KDS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDS_SwiftUIApp.swift
+//  KDS-SwiftUI
+//
+//  Created by BingBing on 2024/10/1.
+//
+
+import SwiftUI
+
+@main
+struct KDS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
